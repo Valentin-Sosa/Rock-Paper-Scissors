882bb679d9e7a260f942a73cd7e1647ffb578607
Valentin-Sosa <valentin.sosa093@gmail.com>
2023-12-21 08:58:19 -0300

    Game over feature added, changed font of h1, more clean code.

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 let playerScore = 0;
 let computerScore = 0;
 
+const main = document.querySelector("main");
 const rockButton = document.querySelector("#rock-button");
 const paperButton = document.querySelector("#paper-button");
 const scissorsButton = document.querySelector("#scissors-button");
-const results = document.querySelector(".results-info");
+const roundResult = document.querySelector(".results-info");
 const computerInfo = document.querySelector(".computer");
 const playerInfo = document.querySelector(".player");
+const reset = document.querySelector(".reset-menu");
 const winner = document.querySelector(".winner");
+const playAgainButtton = document.querySelector(".play-again");
 
-playerInfo.textContent = `${playerScore}`
-computerInfo.textContent = `${computerScore}`;
+reset.style.display = "none";
 
 rockButton.addEventListener('click', () => game("Rock"));
 paperButton.addEventListener('click', () =>game("Paper"));
 scissorsButton.addEventListener('click',() => game("Scissors"));
+playAgainButtton.addEventListener('click', resetGame);
 
 function getComputerChoice()
 {
@@ -40,16 +43,43 @@ function playRound(playerSelection, computerSelection)
        (playerSelection === "scissors" && computerSelection === "paper"))
     {
         playerScore++;
-        results.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
+        roundResult.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
     }
     else if((playerSelection === "rock" && computerSelection === "paper")||
         (playerSelection === "paper" && computerSelection === "scissors")||
         (playerSelection === "scissors" && computerSelection === "rock"))
     {
         computerScore++;
-        results.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
+        roundResult.textContent = `You lost! ${computerSelection} beats ${playerSelection}`;
     }
-    else results.textContent = `It's a tie! ${playerSelection} ties whith ${computerSelection}`;
+    else roundResult.textContent = `It's a tie! ${playerSelection} ties whith ${computerSelection}`;
+}
+
+function checkGameOver()
+{
+    if(playerScore === 5)
+    {
+        main.style.display = "none";
+        reset.style.display = "flex";
+        winner.textContent = "You win!";
+    } 
+    else if(computerScore === 5)
+    {
+        main.style.display = "none";
+        reset.style.display = "flex";
+        winner.textContent = "You lost!";
+    } 
+}
+
+function resetGame()
+{
+    playerScore = 0;
+    computerScore = 0;
+    main.style.display = "block";
+    reset.style.display = "none";
+    roundResult.textContent= "Choose Your Weapon";
+    playerInfo.textContent = "0";
+    computerInfo.textContent = "0";
 }
 
 function game(playerSelection)
@@ -59,7 +89,8 @@ function game(playerSelection)
     playerInfo.textContent = `${playerScore}`
     computerInfo.textContent = `${computerScore}`;
 
-    if(playerScore === 5) winner.textContent = "You win!";
-    else if(computerScore === 5) winner.textContent = "You lose!";
+    checkGameOver();
+
 }
+
 
